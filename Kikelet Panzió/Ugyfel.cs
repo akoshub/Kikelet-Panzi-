075a5c67c455953cce832c2fbcda04c1a634d924
timkoakos <tkos@WINDOWS-FMDS8MV>
2024-05-22 10:21:31 +0200

    Add project files.

---
+++ Kikelet Panzió/Ugyfel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kikelet_Panzió
+{
+    internal class Ugyfel
+    {
+        string azon;
+        string nev;
+        DateTime szuletes;
+        string email;
+        bool vip;
+
+        public Ugyfel(string nev, DateTime szuletes, string email, bool vip)
+        {
+            this.azon = AzonositoLetrehozas(nev);
+            this.Nev = nev;
+            this.Szuletes = szuletes;
+            this.Email = email;
+            this.Vip = vip;
+        }
+
+        public string Azon { get => azon; set => azon = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public DateTime Szuletes { get => szuletes; set => szuletes = value; }
+        public string Email { get => email; set => email = value; }
+        public bool Vip { get => vip; set => vip = value; }
+
+        private string AzonositoLetrehozas(string nev)
+        {
+            return $"{nev}_{DateTime.Now.ToString("yyyyMMddhhmmss")}";
+        }
+
+
+    }
+
+    internal class Szoba
+    {
+        int szobaszam;
+        int ferohelyekszama;
+        int aregyfore;
+
+        public Szoba(int szobaszam, int ferohelyekszama, int aregyfore)
+        {
+            this.Szobaszam = szobaszam;
+            this.Ferohelyekszama = ferohelyekszama;
+            this.Aregyfore = aregyfore;
+        }
+
+        public int Szobaszam
+        {
+            get => szobaszam;
+            set
+            {
+                if (value >= 1 && value <= 6)
+                {
+                    szobaszam = value;
+                }
+            }
+        }
+        public int Ferohelyekszama
+        {
+            get => ferohelyekszama;
+            set
+            {
+                if (value >= 1 && value <= 4)
+                {
+                    ferohelyekszama = value;
+                }
+            }
+        }
+        public int Aregyfore
+        {
+            get => aregyfore;
+            set
+            {
+                if (value >= 6000 && value <= 12000)
+                {
+                    aregyfore = value;
+                }
+            }
+        }
+    }
+}
---
+++ Kikelet Panzió/Ugyfelfelulet.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kikelet_Panzió
+{
+    /// <summary>
+    /// Interaction logic for Ugyfelfelulet.xaml
+    /// </summary>
+    public partial class Ugyfelfelulet : Window
+    {
+        List<Ugyfel> ugyfelek = new List<Ugyfel>();
+        List<Szoba> szobak = new List<Szoba>();
+        
+        public Ugyfelfelulet()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRegiszt_Click(object sender, RoutedEventArgs e)
+        {
+            string nev = TbxNev.Text;
+            DateTime szuletesidatum = DpSzulet.SelectedDate ?? DateTime.MinValue;
+            string email = TbxEmail.Text;
+            bool vip = ChbxVip.IsChecked ?? false;
+            if (!string.IsNullOrEmpty(nev) && szuletesidatum != DateTime.MinValue && !string.IsNullOrEmpty(email))
+            {
+                Ugyfel ujUgyfel = new Ugyfel(nev, szuletesidatum, email, vip);
+            }
+
+        }
+
+        private void ChbxVip_Checked(object sender, RoutedEventArgs e)
+        {
+            //kedvez = Ar * 0.3;
+        }
+    }
+}
